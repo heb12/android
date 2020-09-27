d711429d5478dd237956d51b858dc744f1502ed4
Daniel <brikbusters@gmail.com>
2020-09-27 16:00:08 -0400

    Add just chapter functionality

--- backend/main.c
+++ backend/main.c
@@ -5,59 +5,74 @@
 #include <android/log.h>
 
 #include "biblec/biblec.h"
+#include "biblec/main.h"
 #include "fbrp/reference.h"
 #include "fbrp/fbrp.h"
 
 struct Translation loadedTranslations[10];
 
+// So java wants massive function names. Why. Because Java.
 JNIEXPORT jintArray JNICALL
-Java_com_heb12_android_MainActivity_cgetVerses(JNIEnv* env, jobject thiz, jstring ref) {
+Java_com_heb12_android_MainActivity_cgetVerses
+(JNIEnv* env, jobject thiz, jstring ref) {
 	const char *nativeRef = (*env)->GetStringUTFChars(env, ref, 0);
 
 	int *tryRef;
-	struct Reference reference;
-	parseReference(tryRef, nativeRef, strlen(nativeRef), &reference);
+	struct Reference reference = parseReference(tryRef, nativeRef);
 
-	int to = 1;
-	if (reference.verse[0].r[0] != reference.verse[0].r[1]) {
-		to = reference.verse[0].r[1] - reference.verse[0].r[0] + 1;
+	int verseStart = 1;
+	int verseEnd = 0;
+	if (reference.verseLength != 0) {
+		verseStart = reference.verse[0].r[0];
+		verseEnd = reference.verse[0].r[1];
 	}
 
-	char result[30][600];
-	int tryStruct;
-	getVerses(
-		&tryStruct,
-		result,
-		loadedTranslations[0],
+	int tryReader;
+	struct Reader reader = reader_new(
+		&tryReader,
+		&loadedTranslations[0],
 		reference.book,
 		reference.chapter[0].r[0],
-		reference.verse[0].r[0],
-		to
+		verseStart,
+		verseEnd
 	);
 
-	// Convert native array into java array
+	if (tryReader) {
+		jclass error = (*env)->FindClass(env, "java/lang/IllegalArgumentException");
+		(*env)->ThrowNew(env, error, "Error getting verse.");
+		return 0;
+	}
+
+	// Create a Java array
 	jobjectArray array;
 	array = (*env)->NewObjectArray(
 		env,
-		to,
+		reader.to + 1,
 		(*env)->FindClass(env, "java/lang/String"),
 		(*env)->NewStringUTF(env, "")
-	 );
+	);
 
-	for (int i = 0; i < to; i++) {
+	// Insert verses into Java array
+	int i = 0;
+	while (1) {
+		if (reader_next(&reader)) {break;}
 		(*env)->SetObjectArrayElement(
 			env,
 			array,
 			i,
-			(*env)->NewStringUTF(env, result[i])
+			(*env)->NewStringUTF(env, reader.result)
 		);
+		i++;
 	}
+
+	fclose(reader.file);
 
 	return array;
 }
 
 JNIEXPORT jstring JNICALL
-Java_com_heb12_android_MainActivity_cloadTranslation(JNIEnv* env, jobject thiz, jstring indexLocation, jstring textLocation) {
+Java_com_heb12_android_MainActivity_cloadTranslation
+(JNIEnv* env, jobject thiz, jstring indexLocation, jstring textLocation) {
 	const char *nindexLocation = (*env)->GetStringUTFChars(env, indexLocation, 0);
 	const char *ntextLocation = (*env)->GetStringUTFChars(env, textLocation, 0);
 
@@ -75,9 +90,8 @@ Java_com_heb12_android_MainActivity_cloadTranslation(JNIEnv* env, jobject thiz, 
 	if (fileError) {
 		//__android_log_write(ANDROID_LOG_ERROR, "Heb12", "Error");
 		jclass error = (*env)->FindClass(env, "java/lang/IllegalArgumentException");
-		(*env)->ThrowNew(env, error, "Error");
+		(*env)->ThrowNew(env, error, "Error getting verses.");
 	}
-
 
 	// Why? Because Java.
 	return (*env)->NewStringUTF(env, "Void not working. jstring does.");
